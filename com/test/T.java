8610e02066e0e53e3139ccda9a13c64fa9be3d75
Wayne Zheng <zwee.cn@gmail.com>
2011-11-17 12:26:35 +0800

    Version 0.3, do with function

--- com/test/T.java
+++ com/test/T.java
@@ -1,31 +1,13 @@
 package com.test;
 
+import com.udp.UDPServer;
+
 public class T {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 0x1234;
-		System.out.printf("%x ", (byte)0xc0);
-		byte[] array = intToByteArray(a);
-		for (byte b : array) {
-			System.out.printf("%x ", b);
-		}
-		
-		String[] c = {};
-		System.out.println("Len " + c.length);
-		
-	}
-	
-	public  static byte[] intToByteArray(int s) {
-		byte[] shortBuf = new byte[4];
-		for(int i=0; i<4; i++) {
-			int offset = (shortBuf.length - 1 -i)*8;
-			System.out.println(offset);
-			shortBuf[i] = (byte)((s >>> offset)&0xff);
-		}
-		return shortBuf;
+	public static void main(String args[]) {
+		new UDPServer();
 	}
 }
--- com/udp/UDPServer.java
+++ com/udp/UDPServer.java
@@ -4,7 +4,6 @@ import java.net.*;
 import java.util.Arrays;
 import java.io.*;
 
-import org.xbill.DNS.CNAMERecord;
 import org.xbill.DNS.Lookup;
 import org.xbill.DNS.Record;
 import org.xbill.DNS.Type;
@@ -16,7 +15,6 @@ public class UDPServer {
 	private DatagramSocket dataSocket;
 	private DatagramPacket dataPacket;
 	private byte receiveByte[];
-	private String receiveStr;
 
 	public UDPServer() {
 		Init();
@@ -27,59 +25,19 @@ public class UDPServer {
 			dataSocket = new DatagramSocket(PORT);
 			receiveByte = new byte[1024];
 			dataPacket = new DatagramPacket(receiveByte, receiveByte.length);
-			receiveStr = "";
 			int i = 0;
-			FileOutputStream out;
 			while (i == 0)// 无数据，则循环
 			{
 				try {
-					System.out.println("In while...");
+					System.out.println("Waiting for query...");
 					dataSocket.receive(dataPacket);
 					i = dataPacket.getLength();
 					// 接收数据
 					if (i > 0) {
-						// 指定接收到数据的长度,可使接收数据正常显示,开始时很容易忽略这一点
-						receiveStr = new String(receiveByte, 0, dataPacket.getLength());
-						//System.out.println(receiveStr);
-						StringBuffer queryBuffer = new StringBuffer();
-
-						int head = 0x0c;
-						int len = receiveByte[head];// = receiveStr.charAt(head);
-						int curr = head+1;
-						while (len != 0 && curr < dataPacket.getLength()) {
-							if (curr - head > len) {
-								head = curr;
-								//len = receiveStr.charAt(curr++);
-								len = receiveByte[curr++];
-								if (len != 0) {
-									queryBuffer.append(".");
-								}
-							} else {
-								//queryBuffer.append(receiveStr.charAt(curr++));
-								queryBuffer.append((char)receiveByte[curr++]);
-							}
-						}
-
-						out=new FileOutputStream("E:/query.log"); 
-						out.write(receiveByte, 0, dataPacket.getLength());
-						out.close();
-
 						i = 0;// 循环接收
-
+						StringBuffer queryBuffer = getQuery();
 						System.out.println("query:" + queryBuffer);
-						Record [] aRecords = new Lookup(queryBuffer.toString(), Type.A).run();
-						if (aRecords == null) {
-							System.out.println("There is not A record");
-						} else {
-							System.out.println("Found A record, size:" + aRecords.length);
-							byte[] queryBytes = Arrays.copyOf(receiveByte, dataPacket.getLength());
-							byte[] res = new UDPRes().getResData(queryBytes, aRecords);
-
-							response(res);
-							out = new FileOutputStream("E:/res.log");
-							out.write(res);
-							out.close();
-						}
+						doDNS(queryBuffer);
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -112,6 +70,48 @@ public class UDPServer {
 		System.out.println();
 	}
 
+	public StringBuffer getQuery() throws IOException {
+		StringBuffer queryBuffer = new StringBuffer();
+
+		int head = 0x0c;
+		int len = receiveByte[head];// = receiveStr.charAt(head);
+		int curr = head+1;
+		while (len != 0 && curr < dataPacket.getLength()) {
+			if (curr - head > len) {
+				head = curr;
+				//len = receiveStr.charAt(curr++);
+				len = receiveByte[curr++];
+				if (len != 0) {
+					queryBuffer.append(".");
+				}
+			} else {
+				//queryBuffer.append(receiveStr.charAt(curr++));
+				queryBuffer.append((char)receiveByte[curr++]);
+			}
+		}
+
+		FileOutputStream out=new FileOutputStream("E:/query.log"); 
+		out.write(receiveByte, 0, dataPacket.getLength());
+		out.close();
+		return queryBuffer;
+	}
+	
+	public void doDNS(StringBuffer queryBuffer) throws IOException {
+		System.out.println("query:" + queryBuffer);
+		Record [] aRecords = new Lookup(queryBuffer.toString(), Type.A).run();
+		if (aRecords == null) {
+			System.out.println("There is not A record");
+		} else {
+			System.out.println("Found A record, size:" + aRecords.length);
+			byte[] queryBytes = Arrays.copyOf(receiveByte, dataPacket.getLength());
+			byte[] res = new UDPRes().getResData(queryBytes, aRecords);
+			response(res);
+			FileOutputStream out = new FileOutputStream("E:/res.log");
+			out.write(res);
+			out.close();
+		}
+	}
+	
 	public void response(byte[] res) throws IOException {
 		System.out.println("Response to client: " + dataPacket.getAddress().getHostAddress()
 				+ ", Port：" + dataPacket.getPort());
@@ -119,9 +119,5 @@ public class UDPServer {
 				.getAddress(), dataPacket.getPort());
 		//dp.setData(res);
 		dataSocket.send(dp);
-	}
-
-	public static void main(String args[]) {
-		new UDPServer();
 	}
 }
